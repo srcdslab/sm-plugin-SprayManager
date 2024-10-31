1ec910c2df673228fabedf225aa3e9a6d3684b24
Rushaway <rushaway@hotmail.fr>
2024-10-31 13:57:35 +0100

    feat!: switch database charset to utf8mb4 (#21)
    BREAKING CHANGE: change database tables charset from utf8 to utf8mb4

---
+++ migration/300_character_collate_utf8mb4.sql
@@ -0,0 +1,11 @@
+-- Update spraymanager table
+ALTER TABLE `spraymanager` 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Update sprayblacklist table
+ALTER TABLE `sprayblacklist` 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Update spraynsfwlist table
+ALTER TABLE `spraynsfwlist` 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
